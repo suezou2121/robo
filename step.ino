9b59cc17c216b7bc8e82e2b2c6fa79ddc11d6245
aso <aso@cybernoids.jp>
2013-05-13 13:44:47 +0900

    init

---
+++ step.ino
@@ -0,0 +1,16 @@
+﻿#include <Stepper.h>
+
+#define STEPS 480      // 使用するモータのステップ数
+
+// ピン番号を指定して、stepperクラスのインスタンスを生成
+Stepper stepper(STEPS, 1,3,2, 4);
+
+
+void setup() {
+  stepper.setSpeed(30);          // スピードを30RPMに
+  stepper.step(100);
+}
+
+void loop() {
+
+}
